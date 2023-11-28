8d63cf851930feb4d1d03180c05fa879fb9e8c68
Getulio Gutemberg <getulio.dev@gmail.com>
2023-11-28 16:51:17 -0300

    install ts

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { atom, useAtom } from 'jotai'
 
-export const socket = io('http://localhost:3001')
+export const socket = io('https://virtualized-server.vercel.app/')
 export const charactersAtom = atom([])
 export const SocketManager = () => {
     const [_characters, setCharacters] = useAtom(charactersAtom)
