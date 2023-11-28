d4d5502241c8b3a7c9d1bc18db67c9e85e8ac784
Getulio Gutemberg <getulio.dev@gmail.com>
2023-11-28 14:59:12 -0300

    mk server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import { Server } from 'socket.io';
+
+const io = new Server({
+    cors: {
+        origin: 'http://localhost:5173',
+    },
+});
+
+io.listen(3001);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('hello');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    
+    })
+});
+
+
